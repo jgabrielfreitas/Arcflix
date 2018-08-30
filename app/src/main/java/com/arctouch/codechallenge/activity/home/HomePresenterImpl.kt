09ca0ca0625b6c06e9d30b7bbb13e0bbc9203961
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-08-30 17:52:35 -0300

    remove all relationship between TMDB Service request and Presentation layer

--- app/src/main/java/com/arctouch/codechallenge/activity/home/HomePresenterImpl.kt
+++ app/src/main/java/com/arctouch/codechallenge/activity/home/HomePresenterImpl.kt
@@ -3,25 +3,23 @@ package com.arctouch.codechallenge.activity.home
 import android.os.Bundle
 import com.arctouch.codechallenge.activity.base.BasePresenterImpl
 import com.arctouch.codechallenge.infra.HttpResponseHandler
-import com.arctouch.codechallenge.infra.service.TmdbService
-import com.arctouch.codechallenge.infra.api.TmdbApi
 import com.arctouch.codechallenge.infra.data.Cache
+import com.arctouch.codechallenge.infra.service.tmdb.TmdbService
 import com.arctouch.codechallenge.model.UpcomingMoviesResponse
 import java.lang.Exception
 
 class HomePresenterImpl(var view: HomeView, var serviceApi: TmdbService) : BasePresenterImpl(view),
                                                    HomePresenter, HttpResponseHandler<UpcomingMoviesResponse> {
 
+    private val FIRST_PAGE: Long = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestUpcomingMovies()
     }
 
-    private fun requestUpcomingMovies(page: Long=1) {
-        serviceApi.getUpcomingMovies(httpHandler = this,
-                language = TmdbApi.DEFAULT_LANGUAGE,
-                region = TmdbApi.DEFAULT_REGION,
-                pagePosition = page)
+    private fun requestUpcomingMovies(page: Long = FIRST_PAGE) {
+        serviceApi.getUpcomingMovies(httpHandler = this, pagePosition = page)
     }
 
     override fun onComplete() {
--- app/src/main/java/com/arctouch/codechallenge/di/module/HomeModule.kt
+++ app/src/main/java/com/arctouch/codechallenge/di/module/HomeModule.kt
@@ -4,9 +4,11 @@ import com.arctouch.codechallenge.activity.home.HomeActivity
 import com.arctouch.codechallenge.activity.home.HomePresenter
 import com.arctouch.codechallenge.activity.home.HomePresenterImpl
 import com.arctouch.codechallenge.activity.home.HomeView
-import com.arctouch.codechallenge.infra.service.TmdbService
-import com.arctouch.codechallenge.infra.service.TmdbServiceImpl
+import com.arctouch.codechallenge.infra.service.tmdb.TmdbService
+import com.arctouch.codechallenge.infra.service.tmdb.TmdbServiceImpl
 import com.arctouch.codechallenge.infra.api.TmdbApi
+import com.arctouch.codechallenge.infra.service.LocationConfigurationService
+import com.arctouch.codechallenge.infra.service.tmdb.BrazilianLocationConfigurationService
 import dagger.Module
 import dagger.Provides
 
@@ -14,8 +16,13 @@ import dagger.Provides
 class HomeModule {
 
     @Provides
-    fun providesTmdbService(serviceApi: TmdbApi): TmdbService {
-        return TmdbServiceImpl(serviceApi)
+    fun providesLocationConfiguration(): LocationConfigurationService {
+        return BrazilianLocationConfigurationService()
+    }
+
+    @Provides
+    fun providesTmdbService(serviceApi: TmdbApi, config: LocationConfigurationService): TmdbService {
+        return TmdbServiceImpl(serviceApi, config)
     }
 
     @Provides
