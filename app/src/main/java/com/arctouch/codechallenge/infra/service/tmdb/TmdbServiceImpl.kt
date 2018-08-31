4850ae079677efddaec19a0f79eaf3d314b18059
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-08-31 04:34:23 -0300

    update Tmdb service to support movie details request

--- app/src/main/java/com/arctouch/codechallenge/infra/service/tmdb/TmdbServiceImpl.kt
+++ app/src/main/java/com/arctouch/codechallenge/infra/service/tmdb/TmdbServiceImpl.kt
@@ -5,6 +5,7 @@ import com.arctouch.codechallenge.BuildConfig.BASE_URL
 import com.arctouch.codechallenge.infra.HttpResponseHandler
 import com.arctouch.codechallenge.infra.api.TmdbApi
 import com.arctouch.codechallenge.infra.service.LocationConfigurationService
+import com.arctouch.codechallenge.model.Movie
 import com.arctouch.codechallenge.model.UpcomingMoviesResponse
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -32,5 +33,16 @@ class TmdbServiceImpl(var serviceApi: TmdbApi,
                 .doOnError { httpHandler.onError(it) }
                 .subscribe()
     }
+
+    override fun getRemoteMovieDetails(httpHandler: HttpResponseHandler<Movie>, movieId: Long) {
+        serviceApi.movie(movieId, getKeyApi(), locationConfig.getLanguage())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnComplete { httpHandler.onComplete() }
+                .doOnNext { httpHandler.onReceive(it) }
+                .doOnError { httpHandler.onError(it) }
+                .subscribe()
+    }
+
 
 }
