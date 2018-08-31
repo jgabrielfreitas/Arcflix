f144591932ae2e4a00a404a4470538b5e8581014
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-08-31 04:35:56 -0300

    applying O/C principle

--- app/src/main/java/com/arctouch/codechallenge/activity/home/HomeView.kt
+++ app/src/main/java/com/arctouch/codechallenge/activity/home/HomeView.kt
@@ -1,14 +1,11 @@
 package com.arctouch.codechallenge.activity.home
 
 import com.arctouch.codechallenge.activity.base.BaseView
+import com.arctouch.codechallenge.activity.base.LoaderView
 import com.arctouch.codechallenge.model.Movie
 import java.lang.Exception
 
-interface HomeView : BaseView {
-
-  fun onStartSearch()
-
-  fun onStopSearch()
+interface HomeView : BaseView, LoaderView {
 
   fun addMovies(movies: List<Movie>)
 
