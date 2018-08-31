8bdb322d668d768a95f9f13f062b6c5a6900c844
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-08-31 04:33:51 -0300

    small modifications on Movie model

--- app/src/main/java/com/arctouch/codechallenge/model/Models.kt
+++ app/src/main/java/com/arctouch/codechallenge/model/Models.kt
@@ -18,12 +18,18 @@ data class UpcomingMoviesResponse(
 
 @Parcelize
 data class Movie(
-    val id: Int,
+    val id: Long,
     val title: String,
     val overview: String?,
     val genres: List<Genre>?,
     @Json(name = "genre_ids") val genreIds: List<Int>?,
     @Json(name = "poster_path") val posterPath: String?,
     @Json(name = "backdrop_path") val backdropPath: String?,
-    @Json(name = "release_date") val releaseDate: String?
-) : Parcelable
+    @Json(name = "release_date") val releaseDate: String?,
+    @Json(name = "vote_average") val voteAverage: Double,
+    val runtime: Int?
+) : Parcelable {
+    fun reviewNote() = "$voteAverage/10"
+
+    fun runtime() = "${runtime?.toString() ?: "--"}m"
+}
