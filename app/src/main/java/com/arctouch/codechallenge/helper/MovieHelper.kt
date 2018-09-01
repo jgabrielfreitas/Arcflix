9ef3ffba1a14dcb00e74cb9aa22b98e72a92b47f
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-09-01 18:05:08 -0300

    remove cache dependency

--- app/src/main/java/com/arctouch/codechallenge/helper/MovieHelper.kt
+++ app/src/main/java/com/arctouch/codechallenge/helper/MovieHelper.kt
@@ -2,15 +2,16 @@ package com.arctouch.codechallenge.helper
 
 import com.arctouch.codechallenge.util.DateUtils
 import com.jgabrielfreitas.infrastructure.data.Cache
+import com.jgabrielfreitas.models.Genre
 import com.jgabrielfreitas.models.Movie
 
 class MovieHelper {
 
     fun releaseDate(movie: Movie) = DateUtils().convertStringToDate(movie.releaseDate)
 
-    fun filterMoviesCachedIds(movieList: List<Movie>): List<Movie> {
+    fun filterMoviesCachedIds(movieList: List<Movie>, cachedGenres: List<Genre>): List<Movie> {
         return movieList.map { movie ->
-            movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+            movie.copy(genres = cachedGenres.filter { movie.genreIds?.contains(it.id) == true })
         }
     }
 }
