c79ea2363cf5da0fe9df7041926992b31f626cec
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-09-02 02:10:05 -0300

    add shared preferences wrapper

---
+++ infrastructure/src/main/java/com/jgabrielfreitas/infrastructure/persistence/preference/SharedPreferencesWrapper.kt
@@ -0,0 +1,10 @@
+package com.jgabrielfreitas.infrastructure.persistence.preference
+
+import com.orhanobut.hawk.Hawk
+
+class SharedPreferencesWrapper<T> {
+
+    fun save(key:String, content: T) = Hawk.put(key, content)
+
+    fun get(key:String) : T = Hawk.get(key)
+}
