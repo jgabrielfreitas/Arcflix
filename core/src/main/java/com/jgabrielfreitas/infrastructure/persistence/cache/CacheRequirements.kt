5c81799dc8f51c7a7165e1ee855e023f34a70ec6
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-09-02 02:26:41 -0300

    create a logic wrapper cache content for application at core module

---
+++ core/src/main/java/com/jgabrielfreitas/infrastructure/persistence/cache/CacheRequirements.kt
@@ -0,0 +1,15 @@
+package com.jgabrielfreitas.infrastructure.persistence.cache
+
+import com.jgabrielfreitas.infrastructure.persistence.preference.SharedPreferencesWrapper
+
+
+class CacheRequirements {
+
+    private val sharedPreferencesWrapper = SharedPreferencesWrapper<Boolean>()
+    private val MOVIE_IDS_FLAG = "moviesIds"
+
+    fun isAllRequirementsOk(): Boolean = sharedPreferencesWrapper.get(MOVIE_IDS_FLAG)
+
+    fun markAsOk() = sharedPreferencesWrapper.save(MOVIE_IDS_FLAG, true)
+
+}
